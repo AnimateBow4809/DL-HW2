00d996583f4fe2823aa9cec7ed377c1e5fa1b3fc
AnimateBow4809 <alidashtbozorg709@gmail.com>
2026-04-30 16:09:03 +0330

    changed block implementations

--- models/blocks/base_blocks.py
+++ models/blocks/base_blocks.py
@@ -2,14 +2,17 @@
 
 
 class BasicConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, padding, bias=False):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=False, use_relu=True):
         super().__init__()
-        self.conv = nn.Conv2d(
-            in_channels, out_channels, kernel_size=kernel_size,
-            stride=1, padding=padding, bias=bias
-        )
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                              stride=stride, padding=padding, bias=bias)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-    def forward(self, x):
-        return self.relu(self.bn(self.conv(x)))
+        self.use_relu = use_relu
+        if self.use_relu:
+            self.relu = nn.ReLU(inplace=True)
 
+    def forward(self, x):
+        x = self.bn(self.conv(x))
+        if self.use_relu:
+            x = self.relu(x)
+        return x
--- models/blocks/residual_blocks.py
+++ models/blocks/residual_blocks.py
@@ -1,69 +1,56 @@
-﻿from collections import OrderedDict
+﻿from torch import nn
 
-import torch.nn as nn
+from models.blocks.base_blocks import BasicConv2d
 
 
 class ResidualBlock(nn.Module):
     """
     implementation of residual block using original resnet paper
     """
+
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.main_path = nn.Sequential(OrderedDict([
-            ("conv1", nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)),
-            ("bn1", nn.BatchNorm2d(out_channels)),
-            ("relu1", nn.ReLU(inplace=True)),
-            ("conv2", nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)),
-            ("bn2", nn.BatchNorm2d(out_channels))
-        ]))
+        self.main_path = nn.Sequential(
+            BasicConv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, use_relu=True),
+            BasicConv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, use_relu=False)
+        )
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1,
-                          stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
+            self.shortcut = BasicConv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0,
+                                        use_relu=False)
+
         self.final_relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         identity = self.shortcut(x)
         out = self.main_path(x)
         out += identity
-        out = self.final_relu(out)
-        return out
+        return self.final_relu(out)
 
 
 class Bottleneck(nn.Module):
     """
     implementation of bottleneck block using resnetB from bags of tricks paper
     """
+
     def __init__(self, in_channels, intermediate_channels, out_channels, stride=1):
         super().__init__()
-        self.main_path = nn.Sequential(OrderedDict([
-            ("conv1",
-             nn.Conv2d(in_channels, intermediate_channels, kernel_size=1, stride=1, padding=0, bias=False)),
-            ("bn1", nn.BatchNorm2d(intermediate_channels)),
-            ("relu1", nn.ReLU(inplace=True)),
-            ("conv2",
-             nn.Conv2d(intermediate_channels, intermediate_channels, kernel_size=3, stride=stride, padding=1, bias=False)),
-            ("bn2", nn.BatchNorm2d(intermediate_channels)),
-            ("relu2", nn.ReLU(inplace=True)),
-            ("conv3",
-             nn.Conv2d(intermediate_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)),
-            ("bn3", nn.BatchNorm2d(out_channels)),
-        ]))
+        self.main_path = nn.Sequential(
+            BasicConv2d(in_channels, intermediate_channels, kernel_size=1, stride=1, padding=0, use_relu=True),
+            BasicConv2d(intermediate_channels, intermediate_channels, kernel_size=3, stride=stride, padding=1,
+                        use_relu=True),
+            BasicConv2d(intermediate_channels, out_channels, kernel_size=1, stride=1, padding=0, use_relu=False)
+        )
+
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1,
-                          stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
+            self.shortcut = BasicConv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0,
+                                        use_relu=False)
+
         self.final_relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         identity = self.shortcut(x)
         out = self.main_path(x)
         out += identity
-        out = self.final_relu(out)
-        return out
+        return self.final_relu(out)
